6c72f99d63e9813212ea09328c36ffcfc7fb8a85
akshaykanherkar7 <akshaykanherkar7@gmail.com>
2022-10-11 19:33:50 +0530

    Added Data in Table Format and Search functionality as well

--- src/Pages/StocksPage.jsx
+++ src/Pages/StocksPage.jsx
@@ -1,16 +1,105 @@
-import React, { useEffect } from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Input,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import "./StockPage.css";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getStocksAPI } from "../Redux/Stocks/stocks.action";
 
 const StocksPage = () => {
   const dispatch = useDispatch();
   const { Stocks } = useSelector((state) => state.stocks);
-  console.log("Stocks:", Stocks);
+  const [Data, setData] = useState(Stocks);
+  const [Search, setSearch] = useState([]);
+  const [value, setValue] = useState("");
+  console.log("Data:", Data);
+
+  useEffect(() => {
+    if (Stocks) {
+      setData([...Stocks]);
+    } else if (value === "") {
+      setData([...Stocks]);
+    }
+  }, [Stocks, value]);
+
+  const handleSearch = () => {
+    let newData = Data.filter((el) => {
+      if (el.Symbol === value) {
+        return el;
+      } else if (el.Name.toLowerCase() === value) {
+        return el;
+      }
+    });
+    if (newData) {
+      setSearch([...newData]);
+    }
+  };
+
+  useEffect(() => {
+    if (Search) {
+      setData([...Search]);
+    } else {
+      setData([...Stocks]);
+    }
+  }, [Search]);
 
   useEffect(() => {
     dispatch(getStocksAPI());
   }, []);
-  return <div>StocksPage</div>;
+
+  return (
+    <div>
+      <Heading textAlign={"center"} color="brown">
+        Stocks Page
+      </Heading>
+      <Box w="50%" m="auto" mt="10px">
+        <Flex gap="5px">
+          <Input
+            placeholder="Search by Symbol or Name"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <Button onClick={handleSearch} colorScheme={"teal"}>
+            Search
+          </Button>
+        </Flex>
+      </Box>
+      <TableContainer mt="10px" width={"80%"} m="auto">
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th color={"white"}>Symbol</Th>
+              <Th color={"white"}>Name</Th>
+              <Th color={"white"}>Sector</Th>
+              <Th color={"white"}>Validtill</Th>
+            </Tr>
+          </Thead>
+          <Tbody data-cy="table-body">
+            {Data.length &&
+              Data.map((item) => (
+                <Tr key={item.id}>
+                  <Td>{item.Symbol}</Td>
+                  <Td>{item.Name}</Td>
+                  <Td>{item.Sector}</Td>
+                  <Td>{item.Validtill}</Td>
+                </Tr>
+              ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 };
 
 export default StocksPage;
